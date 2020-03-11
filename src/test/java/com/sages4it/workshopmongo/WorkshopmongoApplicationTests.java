44052e4c93868b66bb93e1763b9a54477cf5d202
srscruz <srscruz13@gmail.com>
2020-03-11 20:18:45 -0300

    Initial commit

---
+++ src/test/java/com/sages4it/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sages4it.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

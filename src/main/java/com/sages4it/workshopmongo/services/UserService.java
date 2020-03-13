68c0b9d4cead2ccd0ca23a4898d8a16bed99a29b
srscruz <srscruz13@gmail.com>
2020-03-13 16:18:11 -0300

    Criada a função INSERT de usuários

--- src/main/java/com/sages4it/workshopmongo/services/UserService.java
+++ src/main/java/com/sages4it/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sages4it.workshopmongo.domain.User;
+import com.sages4it.workshopmongo.dto.UserDTO;
 import com.sages4it.workshopmongo.repository.UserRepository;
 import com.sages4it.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -14,14 +15,21 @@ import com.sages4it.workshopmongo.services.exception.ObjectNotFoundException;
 public class UserService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository repo;
 
 	public List<User> findAll() {
-		return repository.findAll();
+		return repo.findAll();
 
 	}
 	public User findById(String id) {
-		Optional<User> obj = repository.findById(id);
+		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

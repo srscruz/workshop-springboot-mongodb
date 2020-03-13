d31cb16600ff566092f0381f06959084fea2d774
srscruz <srscruz13@gmail.com>
2020-03-13 19:25:29 -0300

    implementaçao do metodo update

--- src/main/java/com/sages4it/workshopmongo/resources/UserResource.java
+++ src/main/java/com/sages4it/workshopmongo/resources/UserResource.java
@@ -30,21 +30,31 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-			return ResponseEntity.ok().body(new UserDTO(obj));
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-			return ResponseEntity.created(uri).build();
-	}	
-	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
-			return ResponseEntity.noContent().build();
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }

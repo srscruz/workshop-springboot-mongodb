dff6cc58fce79cf35f59fad0e413889133dae327
srscruz <srscruz13@gmail.com>
2020-03-16 16:44:35 -0300

    Consulta simples com @Query

--- src/main/java/com/sages4it/workshopmongo/services/PostService.java
+++ src/main/java/com/sages4it/workshopmongo/services/PostService.java
@@ -16,13 +16,13 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 
-
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}
-	public List<Post> findByTitle(String text){
-	 return  repo.findByTitleContainingIgnoreCase(text);	
+
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
 	}
-	
+
 }

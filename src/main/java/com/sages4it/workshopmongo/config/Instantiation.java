7f94eeabe6045a22dc34f20a5cae4bca90b04d7e
srscruz <srscruz13@gmail.com>
2020-03-16 12:50:53 -0300

    Referenciando os posts dos usuários

--- src/main/java/com/sages4it/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sages4it/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.sages4it.workshopmongo.domain.Post;
 import com.sages4it.workshopmongo.domain.User;
+import com.sages4it.workshopmongo.dto.AuthorDTO;
 import com.sages4it.workshopmongo.repository.PostRepository;
 import com.sages4it.workshopmongo.repository.UserRepository;
 @Configuration
@@ -24,7 +25,8 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getDefault());
 		
-		// userRepository.deleteAll();
+	 userRepository.deleteAll();
+	 postRepository.deleteAll();
 		
 		User u1 = new User(null, "Carlos Alberto da Silva", "carlossilva@gmail.com");
 		User u2 = new User(null, "Welington Gonçalves de Souza", "wsouza@gmail.com");
@@ -32,12 +34,15 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		
-		Post post1 = new Post(null, sdf.parse("12/03/2020"), "Corona Virus", "O mundo está complicado!", u2);
-		Post post2 = new Post(null, sdf.parse("13/03/2020"), "Suspensão de Aulas", "Governo do Estado de São Paulo suspenderá as aulas à partir da proxima semana.", u1);
+		Post post1 = new Post(null, sdf.parse("12/03/2020"), "Corona Virus", "O mundo está complicado!", new AuthorDTO(u2));
+		Post post2 = new Post(null, sdf.parse("13/03/2020"), "Suspensão de Aulas", "Governo do Estado de São Paulo suspenderá as aulas à partir da proxima semana.",new AuthorDTO(u1));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		
+	   u1.getPosts().addAll(Arrays.asList(post1, post2));
+	   
+	   userRepository.save(u1);
+	   
 	}
 
 }
--- src/main/java/com/sages4it/workshopmongo/domain/User.java
+++ src/main/java/com/sages4it/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.sages4it.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class User implements Serializable {
@@ -12,6 +15,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 
 	public User() {
 
@@ -48,6 +55,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,4 +87,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
 }

5eee0f3d4b00f4fb8eb000fae4649f99108a59f7
srscruz <srscruz13@gmail.com>
2020-03-16 18:08:25 -0300

    Consulta Consulta com vários critérios

--- src/main/java/com/sages4it/workshopmongo/resources/PostResource.java
+++ src/main/java/com/sages4it/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.sages4it.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
@@ -33,5 +35,15 @@ public class PostResource {
 
 	}
 
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 
 }

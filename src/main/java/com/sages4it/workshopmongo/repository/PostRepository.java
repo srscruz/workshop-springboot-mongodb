fc0f179529f4da49eeca83a793bef31672766d45
srscruz <srscruz13@gmail.com>
2020-03-16 16:10:11 -0300

    Consulta simples com query methods

--- src/main/java/com/sages4it/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/sages4it/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.sages4it.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sages4it.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post,String>{
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/sages4it/workshopmongo/resources/PostResource.java
+++ src/main/java/com/sages4it/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.sages4it.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sages4it.workshopmongo.domain.Post;
+import com.sages4it.workshopmongo.resources.util.URL;
 import com.sages4it.workshopmongo.services.PostService;
 
 @RestController
@@ -20,6 +24,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+
 	}
 
 
